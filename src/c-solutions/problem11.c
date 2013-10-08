66ea50cf55a16552a622a276555e957476adc5ba
Joseph Rivera <jrivera777@gmail.com>
2013-10-08 18:25:40 -0400

    Completed Problem 11. Brute force :(.  Still really fast as it's a small grid. Changed function parameters back to single pointers and did array math to find values in the 2d array. Switched to stdio getline function instead of my bastardized version.

--- src/c-solutions/problem11.c
+++ src/c-solutions/problem11.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define BUFF_SIZE 32
 
@@ -36,24 +37,25 @@ char* get_line(FILE *f)
     return line;
 }
 
-void  load_grid(FILE* f, int **grid, int M, int N)
+void  load_grid(FILE* f, int *grid, int M, int N)
 {
     int i;
     int j;
-    char* line;
+    int len;
+    char *line = NULL;
     for(i = 0; i < M; i++)
     {
-	line = get_line(f);
+        getline(&line, &len, f);
 	char *num = strtok(line, " ");
 	for(j = 0; j < N; j++)
 	{
-	    grid[i][j] = atoi(num);
+	    grid[i * M +j] = atoi(num);
 	    num = strtok(NULL, " ");
 	}
-	free(line);
     }
+    free(line);
 }
-void print_grid(int **grid, int M, int N)
+void print_grid(int *grid, int M, int N)
 {
     int i;
     int j;
@@ -61,7 +63,7 @@ void print_grid(int **grid, int M, int N)
     {
 	for(j = 0; j < N; j++)
 	{
-	    printf("%02d ", grid[i][j]);
+	    printf("%02d ", grid[i * M + j]);
 	}
 	printf("\n");
     }
@@ -101,51 +103,96 @@ int validate_dir(int r, int c, int M, int N, int adjacents, Direction dir)
 		    valid = 0;
 	    break;
 	}
+	case DUL:
+	{
+	    for(i = 0; i < adjacents; i++)
+		if(c - i < 0 || r - i < 0)
+		    valid = 0;
+	    break;
+	}
+	case DUR:
+	{
+	    for(i = 0; i < adjacents; i++)
+		if(c + i >= N || r - i < 0)
+		    valid = 0;
+	    break;
+	}
+	case DDL:
+	{
+	    for(i = 0; i < adjacents; i++)
+		if(c - i < 0 || r + i >= M)
+		    valid = 0;
+	    break;
+	}
+	case DDR:
+	{
+	    for(i = 0; i < adjacents; i++)
+		if(c + i >= N || r + i>= M)
+		    valid = 0;
+	    break;
+	}
 	default:
 	    valid = 0;
     }
     return valid;
 }
 
-unsigned long calc_largest_at(int **grid, int M, int N, int r,
+unsigned long calc_largest_at(int *grid, int M, int N, int r,
 			     int c, Direction dir, int adjacents)
 {
     unsigned long prod = 1;
     int i;
     if(!validate_dir(r, c, M, N, adjacents, dir))
-	return -1;
+	return 0;
+
     switch(dir)
     {
 	case UP:
 	{
 	    for(i = 0; i < adjacents; i++)
-	    {
-		prod *= grid[r-i][c];
-	    }
+		prod *= grid[(r-i) * M + c];
 	    break;
 	}
 	case DOWN:
 	{
 	    for(i = 0; i < adjacents; i++)
-	    {
-		prod *= grid[r+i][c];
-	    }
+		prod *= grid[(r+i) * M + c];
 	    break;
 	}
 	case LEFT:
 	{
 	    for(i = 0; i < adjacents; i++)
-	    {
-		prod *= grid[r][c-i];
-	    }
+		prod *= grid[r * M + (c-i)];
 	    break;
 	}
 	case RIGHT:
 	{
 	    for(i = 0; i < adjacents; i++)
-	    {
-		prod *= grid[r][c+i];
-	    }
+		prod *= grid[r * M + (c+i)];
+	    break;
+	}
+        case DUL:
+	{
+	    for(i = 0; i < adjacents; i++)
+		prod *= grid[(r - i) * M + (c - i)];
+	    break;
+	}
+        case DUR:
+	{
+	    for(i = 0; i < adjacents; i++)
+		prod *= grid[(r - i) * M + (c + i)];
+	    break;
+	}
+        case DDL:
+	{
+	    for(i = 0; i < adjacents; i++)
+		prod *= grid[(r + i) * M + (c - i)];
+	    break;
+	}
+        case DDR:
+	{
+	    for(i = 0; i < adjacents; i++)
+		prod *= grid[(r + i) * M + (c + i)];
 	    break;
 	}
 	default:
@@ -161,8 +208,12 @@ int main(int argc, char **argv)
 {
     FILE *f;
     int M, N, adjacents;
-    int **grid;
-    int i;
+    int *grid;
+    int i, j;
+    clock_t begin, end;
+    double time_spent;
+
+    begin = clock();
 
     if(argc < 5)
     {
@@ -174,9 +225,7 @@ int main(int argc, char **argv)
     N = atoi(argv[3]);
     adjacents = atoi(argv[4]);
 
-    grid = malloc(sizeof(int*) * M);
-    for(i = 0; i < M; i++)
-	grid[i] = malloc(sizeof(int)*N);
+    grid = malloc(sizeof(int*) * M * N);
 
     f = fopen(argv[1], "r");
     if(f == NULL)
@@ -188,10 +237,27 @@ int main(int argc, char **argv)
     load_grid(f, grid, M, N);
     print_grid(grid, M, N);
 
-    unsigned long prod = calc_largest_at(grid, M, N, 1,0, RIGHT, adjacents);
-    printf("Prod = %lu\n", prod);
+    //brute force search.  Not good, but works for this size grid...
+    Direction dir;
+    unsigned long maxProd = 0;
     for(i = 0; i < M; i++)
-	free(grid[i]);
+    {
+        for(j = 0; j < N; j++)
+        {
+            for(dir = 0; dir < 8; dir++)
+            {
+                unsigned long prod = calc_largest_at(grid, M, N, i, j, dir, adjacents);                
+                if(prod > maxProd)
+                    maxProd = prod;
+            }
+        }
+    }
+
     free(grid);
+
+    end = clock();
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Prod = %lu found in %f\n", maxProd, time_spent);
+
     return 0;
 }
