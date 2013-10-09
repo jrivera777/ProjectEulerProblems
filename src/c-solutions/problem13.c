f1e5521b8bea8659865cd2c8963cb1c169ca7712
Joseph Rivera <jrivera777@gmail.com>
2013-10-09 14:39:09 -0400

    Finished Problem 13.

--- src/c-solutions/problem13.c
+++ src/c-solutions/problem13.c
@@ -3,41 +3,56 @@
 
 #define DIGITS 1024
 #define NUM_SIZE 50
-
-int main()
+#define NUM_COUNT 100
+int main(int argc, char **argv)
 {
     int i, j;
+    size_t len = 0;
+    FILE *f;
     int digits[DIGITS] = {0};
-    char *numbers[] = {"37107287533902102798797998220837590246510135740250", "46376937677490009712648124896970078050417018260538"};
+    char *numbers[NUM_COUNT];
     int carry = 0;
     int last_digit = DIGITS - 1;
 
+    if(argc < 2)
+    {
+	printf("Usage: /.problem13 <numberFile>\n");
+	return -1;
+    }
+    f = fopen(argv[1], "r");
+    for(i = 0; i < NUM_COUNT; i++)
+    {
+	numbers[i] = NULL;
+	getline(&numbers[i], &len, f);
+    }
+
+
     for(i = NUM_SIZE - 1; i >= 0; i--)
     {
-        int sum = 0;
-        for(j = 0; j < 2; j++)
-        {
-            sum += numbers[j][i] - '0';
-        }
-        sum += carry;
-        carry = sum / 10;
-        digits[last_digit--] = sum % 10;
+	int sum = 0;
+	for(j = 0; j < NUM_COUNT; j++)
+	{
+	    sum += (numbers[j][i] - '0');
+	}
+	sum += carry;
+	carry = sum / 10;
+	digits[last_digit--] = sum % 10;
     }
     if(carry)
-        digits[last_digit] = carry;
+	digits[last_digit] = carry;
     else
-        last_digit++;
+	last_digit++;
 
     int* ptr = digits;
     i = 0;
     while(*ptr == 0)
     {
-        ptr++;
-        i++;
+	ptr++;
+	i++;
     }
     printf("Sum is: ");
     for(; last_digit < DIGITS; last_digit++)
-        printf("%d", digits[last_digit]);
+	printf("%d", digits[last_digit]);
     printf("\n");
     return 0;
 }
